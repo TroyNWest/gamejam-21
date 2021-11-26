80691f2c529be3af6e86b2978cd20b5c286a25e2
Your Name <you@example.com>
2021-11-26 12:20:35 -0500

    initial commit

--- src/game.ts
+++ src/game.ts
@@ -1,38 +1,42 @@
 import 'phaser';
 
-export default class Demo extends Phaser.Scene
-{
-    constructor ()
-    {
+export default class Demo extends Phaser.Scene {
+    constructor() {
         super('demo');
     }
 
-    preload ()
-    {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+    preload() {
+        this.load.atlas('rogue_atlas', 'assets/tiles/colored_tilemap_packed.png', 'assets/tiles/rogue_atlas.json')
+        // this.load.spritesheet('rogue', 'assets/tilemap/colored_tilemap_packed.png', { frameWidth: 8, frameHeight: 8 })
     }
 
-    create ()
-    {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+    create() {
+        console.log("before atlas!");
 
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+        const atlas = this.add.sprite(400, 300, 'rogue_atlas', 'wall_door_top')
 
-        this.add.image(400, 300, 'libs');
+        // const sprite = this.add.sprite(400, 300, 'rogue', 4);
+        // sprite.frame = 1;
+        atlas.setScale(3, 3);
 
-        const logo = this.add.image(400, 70, 'logo');
 
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
+
+        // this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+
+        // this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+
+        // this.add.image(400, 300, 'libs');
+
+        // const logo = this.add.image(400, 70, 'logo');
+
+        // this.tweens.add({
+        //     targets: logo,
+        //     y: 350,
+        //     duration: 1500,
+        //     ease: 'Sine.inOut',
+        //     yoyo: true,
+        //     repeat: -1
+        // })
     }
 }
 
